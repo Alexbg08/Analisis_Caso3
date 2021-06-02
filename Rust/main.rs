a1ba36eb33e7017cc53e18f42235faaf9d8a46f5
Alexbg08 <1597530a>
2021-06-02 12:00:00 -0600

    Commit 1

--- Rust/main.rs
+++ Rust/main.rs
@@ -5,7 +5,7 @@ const LIST_NUM : i32 = 0;
 const SRCH_NUM : i32 = 1;
 
 
-//Funcion de buscar en la lista
+//Funcion de buscar en la lista en rush
 pub fn linear_search<T: PartialEq>(item: &T, arr: &[T]) -> i32 {
     let mut idx_pos = -1; // -1 indicates not found
 
